e87ee6838c81d072e660a37286f9f0ffb29586be
maftab5 <57639006+maftab5@users.noreply.github.com>
2020-06-14 13:17:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'/build')));
+
+
+// for server
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+app.listen(8000, () => console.log('Listening on port 8000'));
+// to star
