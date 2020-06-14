696c2a8bf3f5d4f1437529bdd20cf8c93d837e65
maftab5 <57639006+maftab5@users.noreply.github.com>
2020-06-14 17:05:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import path from 'path';
-
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-
-app.use(express.static(path.join(__dirname,'/build')));
-app.use(bodyParser.json());
-
-app.get('/hello',(req,res)=> res.send("hello"));
-
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname + '/build/index.html'));
-})
-
-app.listen(5000, ()=> console.log('Listening to port 8000'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
